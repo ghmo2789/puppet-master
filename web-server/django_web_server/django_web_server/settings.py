42a89dc04f96a0df6b6f1198ac1be0b042a1d83c
barwepaul <abarwepaul@gmail.com>
2022-11-14 15:51:00 +0100

    working bootstrap

--- web-server/django_web_server/django_web_server/settings.py
+++ web-server/django_web_server/django_web_server/settings.py
@@ -111,13 +111,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, 'static'),
-    os.path.join(STATIC_ROOT, 'bootstrap')
+    os.path.join(BASE_DIR, 'static/bootstrap'),
+
 ]
 
 # Default primary key field type
--- web-server/django_web_server/website/views.py
+++ web-server/django_web_server/website/views.py
@@ -4,7 +4,29 @@ from django.http import HttpResponse
 
 def index(request):
     dummyData = [{'id': 1, 'os': 'linux', 'status': 'ok'},
+                 {'id': 2, 'os': 'windows', 'status': 'ok'},
+                 {'id': 2, 'os': 'windows', 'status': 'ok'},
+                 {'id': 2, 'os': 'windows', 'status': 'ok'},
+                 {'id': 2, 'os': 'windows', 'status': 'ok'},
+                 {'id': 2, 'os': 'windows', 'status': 'ok'},
+                 {'id': 2, 'os': 'windows', 'status': 'ok'},
+                 {'id': 2, 'os': 'windows', 'status': 'ok'},
+                 {'id': 2, 'os': 'windows', 'status': 'ok'},
+                 {'id': 2, 'os': 'windows', 'status': 'ok'},
+                 {'id': 2, 'os': 'windows', 'status': 'ok'},
+                 {'id': 2, 'os': 'windows', 'status': 'ok'},
+                 {'id': 2, 'os': 'windows', 'status': 'ok'},
+                 {'id': 2, 'os': 'windows', 'status': 'ok'},
+                 {'id': 2, 'os': 'windows', 'status': 'ok'},
+                 {'id': 2, 'os': 'windows', 'status': 'ok'},
+                 {'id': 2, 'os': 'windows', 'status': 'ok'},
+                 {'id': 2, 'os': 'windows', 'status': 'ok'},
+                 {'id': 2, 'os': 'windows', 'status': 'ok'},
+                 {'id': 2, 'os': 'windows', 'status': 'ok'},
+                 {'id': 2, 'os': 'windows', 'status': 'ok'},
+                 {'id': 2, 'os': 'windows', 'status': 'ok'},
                  {'id': 2, 'os': 'windows', 'status': 'ok'}]
+
     context = {'data': dummyData}
     return render(request, 'website/index.html',context)
     #return HttpResponse("Hello world!")
