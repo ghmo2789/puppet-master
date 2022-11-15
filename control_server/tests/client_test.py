c9e65bf0d36db9478fb7ce435f36535f35d470b0
Simon Pettersson <rickebo@rickebo.com>
2022-11-15 14:42:20 +0100

    Updated control server tests to expect dummy data, and to use new router class instead of controller

--- control_server/tests/client_test.py
+++ control_server/tests/client_test.py
@@ -3,20 +3,20 @@ from typing import List
 import pytest
 
 from control_server.tests.utils import get_prefix
-from control_server.src.endpoints.client import get_app
+from control_server.src import router
 
 
 @pytest.fixture
 def app():
-    return get_app()
+    return router.app
 
 
 def test_init(client):
     response = client.post(f"{get_prefix()}/client/init", json={
         "os_name": "1",
         "os_version": "1",
-        "computer_name": "1",
-        "computer_user": "1",
+        "hostname": "1",
+        "host_user": "1",
         "privileges": "1"
     })
 
@@ -35,5 +35,4 @@ def test_task(client):
     })
 
     assert isinstance(response.json, List)
-    assert len(response.json) == 0
     assert response.status_code == 200
