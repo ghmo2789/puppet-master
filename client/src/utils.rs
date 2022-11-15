38f234ad1ee6b553bb0675ab08db21848709beb9
Johan.Lovgren <johan.lovgren.5795@student.uu.se>
2022-11-15 16:59:49 +0100

    Minor refactoring.

--- client/src/utils.rs
+++ client/src/utils.rs
@@ -25,10 +25,7 @@ mod tests {
         let keys = ["os_name", "os_version", "hostname", "host_user", "privileges"];
         let id = get_host_identify();
         for key in keys {
-            if !id.contains(key) {
-                assert!(false);
-            }
+            assert!(id.contains(key));
         }
-        assert!(true);
     }
 }
