e6d3794f501c38d740e135f92a2bc20dc24f6d9c
Iza Alstergren <izaalstergren@hotmail.com>
2022-11-16 14:02:13 +0100

    map without markers

---
+++ web-server/django_web_server/django_web_server/static/stylesheets/website/website.js
@@ -0,0 +1,7 @@
+
+
+var map = L.map('mapid').setView([30, 0], 0.5);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
