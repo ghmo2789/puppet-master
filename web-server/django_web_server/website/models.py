22f0d218fbc55349d06a0e250dc88b97cabc8765
barwepaul <abarwepaul@gmail.com>
2022-11-21 14:38:52 +0100

    added models for clients and sent tasks

--- web-server/django_web_server/website/models.py
+++ web-server/django_web_server/website/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.db.models import Model
 
 # Create your models here.
+class Client(models.Model):
+    client_id = models.PositiveIntegerField(default=0, primary_key=True)
+    os = models.CharField(max_length=7)
+    version = models.PositiveIntegerField()
+    host_name = models.CharField(max_length=50)
+    host_user = models.CharField(max_length=50)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.client_id) 
+
+
+class SentTask(models.Model):
+    id = models.AutoField(primary_key=True)
+    client_id = models.ForeignKey(Client, on_delete=models.CASCADE)
+    start_time = models.PositiveIntegerField(default=0)
+    finish_time = models.PositiveIntegerField(default=0)
+    task_type = models.CharField(max_length=20)
+    task_info = models.CharField(max_length=100)
+
+    def __str__(self):
+        return ("task id = " + str(self.id) + " client id = " + str(self.client_id))
--- web-server/django_web_server/website/views.py
+++ web-server/django_web_server/website/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .forms import clientForm
+from .models import Client
+from .models import SentTask 
 
 def index(request):
+    for i in range(1, 23):
+        current_client = Client(client_id=i, os="linux", version = 510, host_name="dator", host_user="user", status=True)
+        current_client.save()
+
+    new_client = Client(client_id=1337, os="windows", version=10, host_name="dator", host_user="user", status=True)
+    new_client.save()
+    new_task = new_client.senttask_set.create(start_time=1, finish_time=0, task_type="type", task_info="description")
+    #new_task.save() 
+
     dummyData = [{'id': 1, 'os': 'linux', 'version': '5.1', 'hostname': 'skoldator', 'host_user': 'Alfred', 'status': 'ok'},
                  {'id': 2, 'os': 'windows', 'version': '5.1', 'hostname': 'skoldator', 'host_user': 'Alfred', 'status': 'ok'},
                  {'id': 3, 'os': 'windows', 'version': '5.1', 'hostname': 'skoldator', 'host_user': 'Alfred', 'status': 'ok'},
