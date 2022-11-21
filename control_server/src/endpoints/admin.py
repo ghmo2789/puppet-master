8d0b6cf13235105ae220959c8bdcfa2895cf999f
ghmo2789 <gholam.mohammadi.2789@student.uu.se>
2022-11-21 14:37:26 +0100

    Getting client information is implemented.

--- control_server/src/endpoints/admin.py
+++ control_server/src/endpoints/admin.py
@@ -1,17 +1,24 @@
 from flask import request, jsonify
 from control_server.src.controller import controller
 from control_server.src.database.database_collection import DatabaseCollection
+from control_server.src.data.identifying_client_data import IdentifyingClientData
+
 
 def client():
     """
     Endpoint handing the client information request from admin GUI
+    :return: Clients information and a status code for representing the request was successful
+     or not and why it may have been unsuccessful, if clients id is missing or
     """
-    client_id = request.args['id']
-    if client_id is None:
-        return '', 400
+    client_id = request.args.get('id')
+    print(client_id)
+    if client_id is None or len(client_id) == 0:
+        return 'Missing client id', 400
 
-    # Get client information
-    databaseCollection = DatabaseCollection()
-    client_info = controller.db.get_one(databaseCollection.USERS, client_id)
+    client_info = controller.db.get_user(
+        client_id,
+    )
     if client_info is None:
         return 'Wrong client id', 404
+
+    return client_info.serialize(), 200
