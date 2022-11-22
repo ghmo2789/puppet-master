3aefb614ef891bc17ed7d59a676b35036583dd3a
ghmo2789 <gholam.mohammadi.2789@student.uu.se>
2022-11-22 10:31:23 +0100

    Working on all clients.

--- control_server/src/endpoints/admin.py
+++ control_server/src/endpoints/admin.py
@@ -1,10 +1,16 @@
+from typing import cast, List
+
 from flask import request, jsonify
 from control_server.src.controller import controller
 from control_server.src.database.database_collection import DatabaseCollection
 from control_server.src.data.identifying_client_data import IdentifyingClientData
+from control_server.src.data.client_data import ClientData
+from control_server.src.data.deserializable import Deserializable
+from control_server.src.data.serializable import Serializable
 
 
 def client():
+    # TODO: Add authorization check
     """
     Endpoint handing the client information request from admin GUI
     :return: Clients information and a status code for representing the request was successful
@@ -22,3 +28,26 @@ def client():
         return 'Wrong client id', 404
 
     return client_info.serialize(), 200
+
+
+def allclients():
+    # TODO: Add authorization check
+    """
+    Endpoint handling all the clients information request from admin GUI
+    :return:
+    """
+    identifier = {}
+    for key, value in request.args.items():
+        identifier[key] = value
+
+    all_clients = cast(
+        List[ClientData],
+        list(controller.db.get_all(collection=DatabaseCollection.USERS,
+                                   identifier=identifier,
+                                   entry_instance_creator=lambda: cast(Deserializable,
+                                                                       ClientData()))
+             ))
+    print(all_clients)
+    return jsonify({
+        'all_clients': all_clients
+    }), 200
