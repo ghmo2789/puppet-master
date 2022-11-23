261cec6d5b3eb7109946d6a2152874801edd019d
Iza Alstergren <izaalstergren@hotmail.com>
2022-11-23 14:59:19 +0100

    filter existing os

--- web-server/django_web_server/website/filters.py
+++ web-server/django_web_server/website/filters.py
@@ -3,13 +3,8 @@ from .models import Client
 
 
 class ClientFilter(django_filters.FilterSet):
-
-    CHOICES = (
-        ('mac', 'mac'),
-        ('windows', 'windows'),
-        ('linux', 'linux'),
-    )
-
+    existing_os = list(Client.objects.values_list('os_name').distinct())
+    CHOICES = tuple((i[0], i[0]) for i in existing_os)
     os_name = django_filters.ChoiceFilter(choices=CHOICES, empty_label="Select OS")
 
     class Meta:
