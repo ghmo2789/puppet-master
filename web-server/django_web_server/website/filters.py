1b5ac464cb016b2c64c656ce4aeca00e91483f2e
barwepaul <abarwepaul@gmail.com>
2022-11-24 10:03:32 +0100

    fixed database issue

--- web-server/django_web_server/website/filters.py
+++ web-server/django_web_server/website/filters.py
@@ -3,9 +3,13 @@ from .models import Client
 
 
 class ClientFilter(django_filters.FilterSet):
-    existing_os = list(Client.objects.values_list('os_name').distinct())
-    CHOICES = tuple((i[0], i[0]) for i in existing_os)
-    os_name = django_filters.ChoiceFilter(choices=CHOICES, empty_label="Select OS")
+    
+    try:
+        existing_os = list(Client.objects.values_list('os_name').distinct())
+        CHOICES = tuple((i[0], i[0]) for i in existing_os)
+        os_name = django_filters.ChoiceFilter(choices=CHOICES, empty_label="Select OS")
+    except:
+        print("Database not initialized")
 
     class Meta:
         model = Client
