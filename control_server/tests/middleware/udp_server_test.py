4b00e18c94dfdd683c15e08a34202361f80164f2
Simon Pettersson <rickebo@rickebo.com>
2022-11-30 15:00:10 +0100

    Added setting to skip UDP client/server tests on CI

--- control_server/tests/middleware/udp_server_test.py
+++ control_server/tests/middleware/udp_server_test.py
@@ -1,5 +1,8 @@
 import socket
 
+import pytest
+from decouple import config
+
 from control_server.src.middleware.events.udp_receive_event import \
     UdpReceiveEvent
 from control_server.src.middleware.udp_server import UdpServer
@@ -51,6 +54,9 @@ def test_udp_server():
     Tests that the UDP server can receive a message
     :return:
     """
+    if config('CI', default=False, cast=bool):
+        pytest.skip('Skipping UDP tests on CI')
+
     with get_udp_server() as server:
         result = ResultContainer()
         server.receive_event += lambda event: handle_udp_response(event, result)
