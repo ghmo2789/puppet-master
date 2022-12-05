d99eb9e32efd5098f06f629db4420f40e97a1f69
ghmo2789 <gholam.mohammadi.2789@student.uu.se>
2022-12-05 14:57:15 +0100

    added tests for get tasks

--- control_server/tests/admin/task_get_admin_test.py
+++ control_server/tests/admin/task_get_admin_test.py
@@ -8,6 +8,53 @@ from control_server.src.data.client_data import ClientData
 from control_server.src.data.identifying_client_data import IdentifyingClientData
 
 
+def set_user_and_task(client_id: str, task_id: str, db_collection: DatabaseCollection):
+    """
+    Helper function for setting a user and task in the database
+    """
+
+    task = Task(
+        name="0",
+        data="0",
+        min_delay=0,
+        max_delay=0
+    ).with_id()
+    client_task = ClientTask(
+        client_id=client_id,
+        task_id=task_id,
+        task=task,
+    )
+    client_task.set_status(status=TaskStatus.DONE)
+    client_task.set_status_code(status_code=0)
+
+    # Set user in DB
+    client_1 = {
+        "os_name": "1",
+        "os_version": "1",
+        "hostname": "1",
+        "host_user": "1",
+        "privileges": "1",
+    }
+    data = ClientData.load_from_dict(client_1, raise_error=True)
+    new_client = IdentifyingClientData(
+        client_data=data,
+        ip=client_id
+    )
+    new_client.set_id(client_id)
+    controller.db.set_user(
+        user_id=client_id,
+        user=new_client,
+        overwrite=True
+    )
+
+    controller.db.set(
+        collection=db_collection,
+        entry_id=client_task.id,
+        entry=client_task,
+        overwrite=True
+    )
+
+
 def test_get_task_invalid_authorization(client):
     """
     Test the /admin/task get endpoint with missing authorization
@@ -77,46 +124,59 @@ def test_get_task_pending_tasks(client):
 
     assert no_task_response.status_code == 404
 
-    task = Task(
-        name="0",
-        data="0",
-        min_delay=0,
-        max_delay=0
-    ).with_id()
-    client_task = ClientTask(
-        client_id=client_id,
-        task_id=task_id,
-        task=task,
-    )
-    client_task.set_status(status=TaskStatus.DONE)
-    client_task.set_status_code(status_code=0)
+    # Insert a user and task in the DB
+    set_user_and_task(client_id=client_id,
+                      task_id=task_id,
+                      db_collection=DatabaseCollection.USER_TASKS)
 
-    # Set user in DB
-    client_1 = {
-        "os_name": "1",
-        "os_version": "1",
-        "hostname": "1",
-        "host_user": "1",
-        "privileges": "1",
-    }
-    data = ClientData.load_from_dict(client_1, raise_error=True)
-    new_client = IdentifyingClientData(
-        client_data=data,
-        ip=client_id
-    )
-    new_client.set_id(client_id)
-    controller.db.set_user(
-        user_id=client_id,
-        user=new_client,
-        overwrite=True
-    )
+    response = \
+        client.get(f"{get_prefix()}/admin/task",
+                   headers={
+                       "Authorization": controller.settings.admin_key
+                   },
+                   query_string={
+                       "id": client_id,
+                       "task_id": task_id
+                   })
 
-    controller.db.set(
-        collection=DatabaseCollection.USER_TASKS,
-        entry_id=client_task.id,
-        entry=client_task,
-        overwrite=True
-    )
+    pending_tasks = response.json.get("pending_tasks")
+    sent_tasks = response.json.get("sent_tasks")
+
+    pending_client_task = ClientTask().deserialize(pending_tasks[0])
+
+    assert len(sent_tasks) == 1
+    assert len(pending_tasks) == 1
+    assert pending_client_task.id.get('client_id') == client_id
+    assert pending_client_task.id.get('task_id') == task_id
+    assert pending_client_task.status == TaskStatus.DONE
+    assert pending_client_task.status_code == 0
+    assert pending_client_task.get_task_id() == task_id
+
+
+def test_get_task_sent_tasks(client):
+    """
+    Test the /admin/task get endpoint with sent task
+    :param client:
+    :return:
+    """
+    client_id = "1966283-b9b8-4503-a431-6bc39046481f"
+    task_id = "1966284-b9b8-4543-a431-6bc39046481f"
+
+    no_task_response = client.get(f"{get_prefix()}/admin/task",
+                                  headers={
+                                      "Authorization": controller.settings.admin_key
+                                  },
+                                  query_string={
+                                      "id": client_id,
+                                      "task_id": task_id
+                                  })
+
+    assert no_task_response.status_code == 404
+
+    # Insert a user and task in the DB
+    set_user_and_task(client_id=client_id,
+                      task_id=task_id,
+                      db_collection=DatabaseCollection.USER_DONE_TASKS)
 
     response = \
         client.get(f"{get_prefix()}/admin/task",
@@ -129,27 +189,17 @@ def test_get_task_pending_tasks(client):
                    })
 
     pending_task = response.json.get("pending_tasks")
-    sent_tasks = response.json.get("sent_tasks")
+    sent_tasks = response.json.get("sent_tasks")[0]
 
-    pending_client_task = ClientTask().deserialize(pending_task[0])
+    sent_client_task = ClientTask().deserialize(sent_tasks[0])
 
     assert len(sent_tasks) == 1
-    assert len(pending_task) == 1
-    assert pending_client_task.id.get('client_id') == client_id
-    assert pending_client_task.id.get('task_id') == task_id
-    assert pending_client_task.status == TaskStatus.DONE
-    assert pending_client_task.status_code == 0
-    assert pending_client_task.get_task_id() == task_id
-
-
-
-def test_get_task_sent_tasks(client):
-    """
-    Test the /admin/task get endpoint with sent task
-    :param client:
-    :return:
-    """
-    pass
+    assert len(pending_task) == 0
+    assert sent_client_task.id.get('client_id') == client_id
+    assert sent_client_task.id.get('task_id') == task_id
+    assert sent_client_task.status == TaskStatus.DONE
+    assert sent_client_task.status_code == 0
+    assert sent_client_task.get_task_id() == task_id
 
 
 def test_get_task(client):
