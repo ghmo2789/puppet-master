52b3c64e96aa5d5b71d8467c17fd05f12ab28845
Simon Pettersson <rickebo@rickebo.com>
2022-12-14 13:08:46 +0100

    Updated Dockerfile to only build and install client, not run it

--- client/start-container.sh
+++ client/start-container.sh
@@ -4,7 +4,7 @@ ARG_FILE=.env
 SAMPLE_ARG_FILE=sample.env
 
 if [ -f "$ARG_FILE" ]; then
-  export $(cat $ARG_FILE | xargs) && rails c
+  export $(grep -v '^#' .env | xargs)
 else
   echo No .env file found. Please create one based on the sample.env file.
   exit 1
