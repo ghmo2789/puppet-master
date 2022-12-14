7fd5f81c89de2040c4a0145603f9fac17c78ba65
izaalstergren <izaalstergren@hotmail.com>
2022-12-14 15:32:39 +0100

    removed old file

--- web-server/django_web_server/website/signals.py
+++
@@ -1,8 +0,0 @@
-from django.db.models.signals import post_save
-from notifications.signals import notify
-from .models import SentTask, Client
-
-def my_handler(sender, instance, created, **kwargs):
-    notify.send(instance, verb='senttask saved')
-
-post_save.connect(my_handler, sender=SentTask)
